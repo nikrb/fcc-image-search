2ca7fe3d09ea557bada2ce936e4b18c6ed39d4bb
nikrb <nik_rb@yahoo.com>
2017-08-03 21:05:55 +0100

    code fetch and history

--- server.js
+++ server.js
@@ -19,16 +19,34 @@ mongo.connect( 'mongodb://localhost:27017/imgs', ( err, db) => {
   });
 
   app.get( '/api/imagesearch/:search_text', (req, res) => {
-    res.send( {result: req.query, text: req.params.search_text});
-    // fetch( 'https://pixabay.com/api?key='+process.env.API_KEY+'&per_page=3')
-    // 	.then(res => res.json())
-    // 	.then(json => console.log(json));
-
+    const search_text = req.params.search_text;
+    if( search_text){
+      let url = 'https://pixabay.com/api?key='+process.env.API_KEY+
+        "&q="+encodeURIComponent( search_text);
+      if( req.query.per_page){
+        url = url.concat( "&per_page=", req.query.per_page);
+      }
+      if( req.query.offset){
+        url = url.concat( "&page=", req.query.offset);
+      }
+      fetch( url)
+        .then(response => response.json())
+        .then( (json) => {
+          const result = json.hits.map( (d) => {
+            return { url: d.previewURL, tags: d.tags, page_url: d.pageURL};
+          });
+          res.send( {url: url, result: result});
+          db.collection( 'search_list').insert( { term: search_text, when: new Date()});
+        });
+    } else {
+      res.send( {result: "search text missing"});
+    }
   });
 
-  app.get( '/api/latest', (req, res) => {
+  app.get( '/api/latest/imagesearch', (req, res) => {
     db.collection( 'search_list').find().toArray( (err, docs) => {
-      res.send( docs);
+      const dox = docs.map( d => ({term:d.term, when:d.when}));
+      res.send( dox);
     });
   });
 
